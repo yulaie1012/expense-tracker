91bf3f3c5c66613fd17a99bb14ff5d279b41ad3c
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-30 15:05:30 +0800

    feat: add login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,12 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+router.post('/login', (req, res) => {
+  console.log(req.body)
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
