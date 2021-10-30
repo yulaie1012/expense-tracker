4b6b14eab61ac3223b55692356f656aeac6e3c82
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-30 10:16:06 +0800

    modify express routes

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
+
+const routes = require('./routes')
+
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`)
