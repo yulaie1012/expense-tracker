fc920170bf05512c9b8603a4086d8cea355111bb
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-30 15:45:16 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
@@ -24,6 +25,8 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,22 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('passport')
+
 const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', (req, res) => {
-  console.log(req.body)
-})
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
 router.post('/register', (req, res) => {
-  // 取得註冊表單參數
   const { name, email, password, confirmPassword } = req.body
 
   User
