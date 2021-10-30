fccd758640ef2b16fb660e20a351257391b340bb
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-30 14:45:01 +0800

    feat: add user router module

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const records = require('./modules/records')
+const users = require('./modules/users')
 
+router.use('/users', users)
 router.use('/records', records)
 router.use('/', home)
 
